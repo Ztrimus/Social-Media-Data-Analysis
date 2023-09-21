a4160a476f4579059339d24a3bd3ebdb693a14d3
Saurabh Zinjad <zinjadsaurabh1997@gmail.com>
2023-09-21 07:08:40 -0700

    ENH: Improve data crawling process

--- src/data_crawling.py
+++ src/data_crawling.py
@@ -8,28 +8,27 @@ Email: zinjadsaurabh1997@gmail.com
 Copyright (c) 2023 Saurabh Zinjad
 """
 from mastodon import Mastodon
-import local_config
+import credentials
+import config
 import utils
 import json
 
-mastodon = Mastodon(
-    client_id=local_config.CLIENT_ID,
-    client_secret=local_config.CLIENT_SECRET,
-    access_token=local_config.ACCESS_TOKEN,
-    api_base_url=local_config.API_BASE_URL,
-)
-
-search_query = [
-    "AIethics",
-    "AIRegulation",
-    "Artificial intelligence",
-    "AI",
-]  # "SocialSciences", "ml", "DataScience", "statistics"
-
-unique_users = utils.get_unique_users(mastodon, search_query)
-
-# Serialize and save the dictionary to a text file
-with open("../data/data.json", "w", encoding="utf-8") as file:
-    json.dump(
-        list(unique_users.values()), file, ensure_ascii=False, indent=4, default=str
+try:
+    mastodon_server = Mastodon(
+        client_id=credentials.CLIENT_ID,
+        client_secret=credentials.CLIENT_SECRET,
+        access_token=credentials.ACCESS_TOKEN,
+        api_base_url=credentials.API_BASE_URL,
     )
+    print("\n\nmastodon server connected.")
+
+    search_query = config.QUERY
+
+    unique_users = utils.unique_users_data_crawling(mastodon_server, search_query)
+
+    # Serialize and save the dictionary to a text file
+    with open(config.JSON_DATA_PATH, "w", encoding="utf-8") as file:
+        json.dump(unique_users, file, ensure_ascii=False, indent=4)  # , default=str
+except Exception as e:
+    # Handle the exception
+    print(f"An exception occurred: {e}")
